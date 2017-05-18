1c12249fe0493d91adb3a653a6ea66da473fb6e9
tt <treasuresss@126.com>
2017-05-18 11:31:58 +0800

    fix: save to csv with bzip2 while encoding is utf8

--- stock/get_finance_news.stock.py
+++ stock/get_finance_news.stock.py
@@ -3,6 +3,7 @@
 
 import tushare as ts
 import sys
+import gzip, bz2
 
 newsDF = ts.get_latest_news(top=1000, show_content=True)
 # print newsDF.columns #Index([u'classify', u'title', u'time', u'url', u'content'], dtype='object')
@@ -14,5 +15,5 @@ assert(len(monthday)==5 and monthday[:2].isdigit() and monthday[2]=='-' and mont
 
 output_file = 'data/finance_news/finance_news.%s.csv.bz2'%(yesterday)
 target = newsDF[newsDF['time'].map(lambda x:x.split()[0])==monthday]
-target.to_csv(output_file, index=False, encoding='utf-8', compression='bz2')
+target.to_csv(bz2.BZ2File(output_file, "w"), index=False, encoding='utf-8')
 print "save %d finance news(%s) to %s "%(len(target), yesterday, output_file)
